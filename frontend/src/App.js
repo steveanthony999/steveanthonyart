ddd9716673b7a8468b7b0fcdbf4c93f8a2eb5e37
Steven Woodward <steveanthony999@gmail.com>
2021-02-09 23:32:03 -0800

    set up footer, header, and navbar. installed bootstrap and linked fontawesome cdn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+
 const App = () => {
-  return <div>hi app</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>hi app</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
