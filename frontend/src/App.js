a3454ccbc4f3c16623e8a4992bd16ea10b52b416
Steven Woodward <steveanthony999@gmail.com>
2021-02-10 23:40:56 -0800

    added product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
