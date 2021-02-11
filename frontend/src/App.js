64270f67916a4d864b29079b24e132f698c8d28d
Steven Woodward <steveanthony999@gmail.com>
2021-02-10 23:10:23 -0800

    implemented react-router and react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+
+// Components
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,3 +1,4 @@
+import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
@@ -5,16 +6,22 @@ const Header = () => {
     <header>
       <Navbar expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Steve Anthony</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Steve Anthony</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/Cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
