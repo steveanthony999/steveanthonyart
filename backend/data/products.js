8e4317e491e52074fa745c4e35bf8ed1be1901ec
Steven Woodward <steveanthony999@gmail.com>
2021-02-11 23:21:22 -0800

    set up backend with express, created simple api

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Power and Control',
+    image: '/images/01.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Open Edition Print',
+    price: 89.99,
+    countInStock: 1000,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'I Scream Green',
+    image: '/images/02.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Original',
+    price: 599.99,
+    countInStock: 1,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Swallowed',
+    image: '/images/03.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Original',
+    price: 929.99,
+    countInStock: 1,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Plain Pain',
+    image: '/images/04.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Limited Edition Giclee',
+    price: 399.99,
+    countInStock: 25,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'A Portrait of Goose',
+    image: '/images/05.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Open Edition Print',
+    price: 49.99,
+    countInStock: 700,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Elephant',
+    image: '/images/06.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nisl id nisi hendrerit placerat ut nec elit. Donec elit lectus, sodales quis dignissim in, pretium ac leo. Nulla vitae ex vel lacus cursus gravida a in purus. Duis egestas vitae enim vitae tincidunt. Nam et nibh quis orci pulvinar malesuada. Nullam ac purus facilisis, semper odio eu, facilisis ligula. Sed a libero velit.',
+    media: 'Oil on canvas',
+    category: 'Original',
+    price: 29.99,
+    countInStock: 0,
+    rating: 5,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
